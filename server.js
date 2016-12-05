1112a3d72cbb439a456475820ddd1a879d0f98b4
Nabeel Khan <rockystorm@gmail.com>
2016-12-04 19:59:35 -0800

    Created MongdoDB account and connected it using server.js

--- server.js
+++ server.js
@@ -7,3 +7,13 @@ const pug = require('pug');
 app.set('view engine', 'pug')
 app.use(express.static('public'))
 
+var db
+var ObjectID = require('mongodb').ObjectID;
+
+MongoClient.connect('mongodb://mnkhan:testing_password@ds119578.mlab.com:19578/dribblebeat-1', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
