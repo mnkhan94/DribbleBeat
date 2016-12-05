f02f3660746299ca2911d8ea2376cb1ca9cf232c
Nabeel Khan <rockystorm@gmail.com>
2016-12-04 20:02:07 -0800

    Added Body-Parser to parse json requests and responses

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const bodyParser= require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 const pug = require('pug');
