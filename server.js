ec94ad755ac23c82fc6bd4316374a5a169c9e078
Nabeel Khan <rockystorm@gmail.com>
2016-12-04 19:57:25 -0800

    Installed MongdoDB using npm

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
 const app = express();
-
+const MongoClient = require('mongodb').MongoClient
 const pug = require('pug');
 
 app.set('view engine', 'pug')
+app.use(express.static('public'))
+
