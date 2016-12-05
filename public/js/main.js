745e273b0fd3908063b281f631c539e42ed1c0d7
Nabeel Khan <rockystorm@gmail.com>
2016-12-04 20:22:29 -0800

    Added Geocomplete to form

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+
+var del = $('.delete-this')
+
+del.on('click', function () {
+  id = $(this).parent().parent().data("id").replace(/\"/g, "")
+  console.log(id)
+  fetch('quotes', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'id': id
+    })
+  }).then(function (response) {
+    window.location.reload()
+  })
+})
