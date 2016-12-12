2066cb2a294b4309fbe8e9a93e02542c156534b4
Nabeel Khan <rockystorm@gmail.com>
2016-12-11 18:42:26 -0800

    Added Passport.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-    'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://mnkhan:testing_password@ds119578.mlab.com:19578/dribblebeat-1' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
--- server.js
+++ server.js
@@ -1,17 +1,48 @@
 const express = require('express');
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient
 const pug = require('pug');
 const exphbs  = require('express-handlebars');
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
-app.use(express.static('public'))
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var session      = require('express-session');
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+    app.use(morgan('dev')); // log every request to the console
+    app.use(cookieParser()); // read cookies (needed for auth)
+
+    app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
+
+    app.use(express.static('public'))
+
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+
+// required for passport
+    app.use(session({ secret: 'session_secret' })); // session secret
+    app.use(passport.initialize());
+    app.use(passport.session()); // persistent login sessions
+    app.use(flash()); // use connect-flash for flash messages stored in session
+
+// Database ====================================================================
+    var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+    mongoose.connect(configDB.url); // connect to our database
+
+// routes ======================================================================
+    require('./routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
 
 var db
 var ObjectID = require('mongodb').ObjectID;
